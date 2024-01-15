d8384be668a3d401ea772294ca1e0b531f13da19
Otso-Veistera <otso.veistera@eduespoo.fi>
2024-01-15 20:11:54 +0200

    Changed title and ref to ico

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,11 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Veistera.com</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="public\favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Veistera.com" />
         <p className="description">
         <code>Under construction</code>
         </p>
