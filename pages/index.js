65b822be9fbb0a6c4a675d50a71401ff45240319
Otso-Veistera <otso.veistera@eduespoo.fi>
2024-01-15 19:31:17 +0200

    Add video, delete footer, change text, change favicon

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,34 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Veistera.com</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+           Under <code>Construction</code>
         </p>
+
+        {/* Video Element */}
+        <video
+          controls
+          autoPlay
+          loop
+          muted
+          style={{ width: '100%', maxHeight: '360px' }}
+        >
+          <source src="/video.mp4" type="video/mp4" />
+          Construction is happening.
+        </video>
       </main>
 
-      <Footer />
     </div>
-  )
+  );
 }
