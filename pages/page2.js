8213aa64d8d95c555879119f1a81e61e0e0db3f6
Otso-Veistera <otso.veistera@eduespoo.fi>
2024-01-21 21:23:31 +0200

    make second test page

---
+++ pages/page2.js
@@ -0,0 +1,29 @@
+// pages/Page2.js
+import Head from 'next/head';
+import Header from '@components/header';
+import Footer from '@components/Footer';
+
+const Page2 = () => {
+  return (
+    <div className="container">
+      <Head>
+        <title>Veistera.com - Page 2</title>
+        {/* Additional meta tags, link tags, etc., can be added here */}
+      </Head>
+
+      <main>
+        <Header title="Veistera.com - Page 2" />
+        <p className="description">
+          {/* Your page content goes here */}
+          <code>Page 2 content</code>
+        </p>
+
+        {/* Additional content for Page 2 goes here */}
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Page2;
